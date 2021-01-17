5b2e4820227cbeef7766d1625ba347b80c5b91a3
Alan Jhone Oliveira <alan.jesus@meta.com.br>
2021-01-17 18:40:35 -0300

    Add Configuação JSF

---
+++ src/main/java/com/esigsoftware/desafio/config/WebJsfConfig.java
@@ -0,0 +1,40 @@
+package com.esigsoftware.desafio.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
+@Configuration
+public class WebJsfConfig implements WebMvcConfigurer, ServletContextAware {
+
+    @Bean
+    public ServletRegistrationBean<FacesServlet> facesServletServletRegistrationBean() {
+        ServletRegistrationBean<FacesServlet> registrationBean = new ServletRegistrationBean<>(
+                new FacesServlet(),"*.jsf");
+        registrationBean.setName("facesServlet");
+        registrationBean.setLoadOnStartup(1);
+
+        return registrationBean;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", "TRUE");
+        servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+        servletContext.setInitParameter("primefaces.THEME","omega");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.jsf");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+}
