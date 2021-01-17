9335e06857a2811f59b858131a9cf75cbdb5d8dc
Alan Jhone Oliveira <alan.jesus@meta.com.br>
2021-01-17 18:38:27 -0300

    Commit Inicial

---
+++ src/test/java/com/esigsoftware/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esigsoftware.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
